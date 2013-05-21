f37347053d599658761c8b185a6a8ae88b3407ed
vkolodrevskiy <viktor.kolodrevskiy@gmail.com>
2013-05-21 14:23:42 +0300

    get rid of @Controller annotation

--- src/main/java/com/vk/rest/controllers/BallController.java
+++ src/main/java/com/vk/rest/controllers/BallController.java
@@ -3,10 +3,8 @@ package com.vk.rest.controllers;
 import com.vk.dao.domain.Ball;
 import com.vk.exception.AppException;
 import com.vk.service.BallManager;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import javax.inject.Inject;
 import java.util.List;
 
 /**
@@ -14,10 +12,8 @@ import java.util.List;
  *
  * @author vkolodrevskiy
  */
-@Controller
 @RequestMapping("/ball")
 public class BallController {
-    @Inject
     private BallManager ballManager;
 
     @RequestMapping(value="list", method=RequestMethod.GET, produces="application/json")
@@ -34,5 +30,9 @@ public class BallController {
     @ExceptionHandler(AppException.class)
     public @ResponseBody String handle(AppException e) {
         return "{\"error\":\"" + e.getMessage() + "\"}";
+    }
+
+    public void setBallManager(BallManager ballManager) {
+        this.ballManager = ballManager;
     }
 }
